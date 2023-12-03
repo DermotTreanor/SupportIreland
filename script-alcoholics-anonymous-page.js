1f2038de884e4e41fd4633e02817853529a0d64d
Dermot Treanor <dermottreanor@google.com>
2023-12-03 22:18:15 +0000

    Added the div under the heading in the HTML. Set the heading container to have a flex-direction of column and the new heading-underline styles were inserted in the CSS. Added in a JS file that has an event listener that will add a class the heading-underline div -- this will have the style which causes the animation.

---
+++ script-alcoholics-anonymous-page.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let headingElement = document.getElementsByClassName("heading-underline")[0]
+    headingElement.classList.add("heading-underline-post-animation")
+})
