eb8492b33eaf96c8cfd882b6b7f5a6a828a95ba7
Dermot Treanor <dermottreanor@google.com>
2023-12-07 09:43:35 +0000

    Changed the script file to use a setTimeout function so that the transition of the underline width would work.

--- script-alcoholics-anonymous-page.js
+++ script-alcoholics-anonymous-page.js
@@ -1,4 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let headingElement = document.getElementsByClassName("heading-underline")[0]
-    headingElement.classList.add("heading-underline-post-animation")
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//     let headingElement = document.getElementsByClassName("heading-underline")[0]
+//     headingElement.classList.add("heading-underline-post-animation")
+// })
+
+setTimeout( () => {
+let headingElement = document.getElementsByClassName("heading-underline")[0]
+headingElement.classList.add("heading-underline-post-animation")
+}, 1);
