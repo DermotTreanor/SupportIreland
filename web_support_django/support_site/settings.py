046a2e9d7172b55ad016e824b3f75beed310132b
DermotTreanor <dermot.treanor1@gmail.com>
2023-12-24 18:20:40 +0000

    First view is up and running as the initial adjustments were made to the project url file, the app url file was created and adjusted too, and the app was installed in the project settings file along with the view itself being created in the view.py ffile in the app folder.

--- web_support_django/support_site/settings.py
+++ web_support_django/support_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'support_ireland',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
