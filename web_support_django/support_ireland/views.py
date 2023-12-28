5bef3b2bd44a4b9c1b91e98d7396a87dc9c41971
DermotTreanor <dermot.treanor1@gmail.com>
2023-12-28 19:50:46 +0000

    Have now set the boiler-plate template to be returned by having the index view in the app return the result of a render function that takes in the current request and the 'support_ireland/index.html' template.

--- web_support_django/support_ireland/views.py
+++ web_support_django/support_ireland/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Hello, world!!!!!</h2>")
+    # return HttpResponse("<h2>Hello, world!!!!!</h2>")
+    return render(request, "support_ireland/index.html")
